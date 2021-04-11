97099eb2041d133b0ed049bd2b1c480564e8a4ab
kurbaniak@student.agh.edu.pl <kurbaniak@student.agh.edu.pl>
2021-04-11 23:23:14 +0200

    Czemu to nie dziaaaa :(

--- google/vue.js
+++ google/vue.js
@@ -3,10 +3,11 @@ var myBody = document.getElementsByTagName('body')[0];
 const input = document.querySelector('input');
 input.addEventListener('change', updateValue);
 
-function updateValue(e)  // działa przy kliknięciu entera i przycisku Szukaj w Google
+function updateValue(e)
 {
     if(e.target.value != '')
-    {
+    {  
+        myBody.classList.remove('home');
         myBody.classList.toggle('results');
     }
     else
@@ -14,5 +15,3 @@ function updateValue(e)  // działa przy kliknięciu entera i przycisku Szukaj w
         myBody.classList.remove('results');
     }
 }
-
-// https://forum.vuejs.org/t/how-do-i-add-remove-classes-to-body/1219/15
