c805ad773fd3ad749b4fdf3894d4a52875038d62
kurbaniak@student.agh.edu.pl <kurbaniak@student.agh.edu.pl>
2021-04-12 14:22:32 +0200

    :))))))))))

--- google/vue.js
+++ google/vue.js
@@ -1,17 +1,6 @@
-var myBody = document.getElementsByTagName('body')[0];
-
-const input = document.querySelector('input');
-input.addEventListener('change', updateValue);
-
-function updateValue(e)
-{
-    if(e.target.value != '')
-    {  
-        myBody.classList.remove('home');
-        myBody.classList.toggle('results');
+var app = new Vue({
+    el: '#app',
+    data: {
+        googleSearch: ""
     }
-    else
-    {
-        myBody.classList.remove('results');
-    }
-}
+});
