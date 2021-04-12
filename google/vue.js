5aa9b7f12c80302b9316802b83400756d7e0f95f
kurbaniak@student.agh.edu.pl <kurbaniak@student.agh.edu.pl>
2021-04-12 16:02:54 +0200

    UGH

--- google/vue.js
+++ google/vue.js
@@ -1,6 +1,16 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        googleSearch: ""
+var myBody = document.getElementsByTagName('body')[0];
+
+const input = document.querySelector('.wpis');
+input.addEventListener('change', updateValue);
+
+function updateValue(e) 
+{
+    if(e.target.value != '')
+    {
+        myBody.classList.toggle('results');
     }
-});
+    else
+    {
+        myBody.classList.remove('results');
+    }
+}
