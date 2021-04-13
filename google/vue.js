36f9a372522dd34fba929e75d2054bf772f39470
kurbaniak@student.agh.edu.pl <kurbaniak@student.agh.edu.pl>
2021-04-13 17:14:10 +0200

    test

--- google/vue.js
+++ google/vue.js
@@ -1,16 +1,6 @@
-var myBody = document.getElementsByTagName('body')[0];
-
-const input = document.querySelector('.wpis');
-input.addEventListener('change', updateValue);
-
-function updateValue(e) 
-{
-    if(e.target.value != '')
-    {
-        myBody.classList.toggle('results');
+var app = new Vue({
+    el: "#app",
+    data: {
+        googleSearch: "",
     }
-    else
-    {
-        myBody.classList.remove('results');
-    }
-}
+});
