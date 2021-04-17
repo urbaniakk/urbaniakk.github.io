97b0c57e487daee10c48fc25e3dc9424e5071223
kurbaniak@student.agh.edu.pl <kurbaniak@student.agh.edu.pl>
2021-04-17 15:41:41 +0200

    test2

--- google/vue.js
+++ google/vue.js
@@ -2,5 +2,22 @@ var app = new Vue({
     el: "#app",
     data: {
         googleSearch: "",
-    }
+    },
+    updated() {
+
+        this.$nextTick(() => {
+        
+        if (this.googleSearch.length > 0) {
+        
+        this.$refs.second.focus();
+        
+        } else {
+        
+        this.$refs.first.focus();
+        
+        }
+        
+        });
+        
+        }
 });
