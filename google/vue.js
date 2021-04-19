da118be8b96021b02cb15f35275fed46c329b2a7
kurbaniak@student.agh.edu.pl <kurbaniak@student.agh.edu.pl>
2021-04-19 18:51:27 +0200

    dziala

--- google/vue.js
+++ google/vue.js
@@ -2,22 +2,24 @@ var app = new Vue({
     el: "#app",
     data: {
         googleSearch: "",
+        cities: window.cities
     },
     updated() {
-
         this.$nextTick(() => {
-        
-        if (this.googleSearch.length > 0) {
-        
+        if (this.googleSearch.length > 0) {        
         this.$refs.second.focus();
-        
         } else {
-        
         this.$refs.first.focus();
-        
         }
-        
         });
-        
+        },
+    computed: {
+        filteredCities: function() {
+            let m = this.cities.filter(city => city.name.includes(this.googleSearch))
+            if(m.length > 10){
+                return m.slice(0,11);
+            }
+            return m
+            }
         }
 });
