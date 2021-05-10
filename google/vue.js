e58f9c654e44649a8ce4acbc13c0d128bbcc2d24
kurbaniak@student.agh.edu.pl <kurbaniak@student.agh.edu.pl>
2021-05-10 17:50:27 +0200

    strzalki prawie dzialajace

--- google/vue.js
+++ google/vue.js
@@ -4,26 +4,56 @@ var app = new Vue({
         googleSearch: "",
         cities: window.cities,
         isActive: 0,
-        control: 0
+        control: 0,
+
+        filteredCities:"",
+        update_filteredCities:true,
+        focused: false,
+        change: false,
+        inFocus: -1,
+        searchedInput:''
     },
     updated() {
         this.$nextTick(() => {
-        if (this.googleSearch.length > 0) {        
-        this.$refs.second.focus();
-        } else {
-        this.$refs.first.focus();
+        if (this.googleSearch.length > 0) 
+        {        
+            this.$refs.second.focus();
+        } 
+        else
+        {
+            this.$refs.first.focus();
         }
         });
-        },
-    computed: {
+    },
+    /*computed: {
         filteredCities: function() {
             let m = this.cities.filter(city => city.name.includes(this.googleSearch))
-            if(m.length > 10){
+            if(m.length > 10)
+            {
                 return m.slice(0,11);
             }
             return m
-            }
+        }
+    },*/
+
+    watch:{
+        inFocus: function () 
+        {
+            this.update_filteredCities = false;
+            this.googleSearch=this.filteredCities[this.inFocus].name;
         },
+        googleSearch: function()
+        {
+            this.createFilteredCities(this.update_filteredCities);
+            this.update_filteredCities=true;
+            console.log(this.filteredCities);
+
+            if(this.inFocus == -1)
+            {
+                this.searchedInput=this.googleSearch;
+            }
+        }
+    },
 
     methods:{
         zmiana: function(a)
@@ -50,6 +80,47 @@ var app = new Vue({
         ustaw: function()
         {
             this.control = 1;
+        },
+        enter: function(){
+            this.update_filteredCities = true;
+            this.change = true;
+            this.inFocus = -1;
+            this.focused = false;
+        },
+        down: function(){
+            if(this.inFocus < this.filteredCities.length-1)
+            {
+                this.inFocus += 1; 
+            }
+            if(this.inFocus == this.filteredCities.length-1)
+            {
+                this.inFocus = 0; 
+            }
+        },
+        up: function(){
+            if(this.inFocus > 0)
+            {
+                this.inFocus -= 1; 
+            }
+            if(this.inFocus == 0)
+            {
+                this.inFocus = this.filteredCities.length-1;
+            }
+        },
+        createFilteredCities: function(yes){
+            if(yes)
+            {
+                let result = this.cities.filter(city => city.name.includes(this.googleSearch));
+                if(result.length > 10)
+                {
+                    this.filteredCities = result.slice(1, 11);
+                }
+                else
+                {
+                    this.filteredCities = result;
+                }
+                this.inFocus = -1;
+            }   
         }
     }
 });
