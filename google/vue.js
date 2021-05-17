07722209dc4e3a959cceb0e193a36d88596968fd
Wojciech Dlubacz <wdlubacz@zarz.agh.edu.pl>
2021-05-17 20:52:12 +0200

    V-autocompleter initial commit

--- google/vue.js
+++ google/vue.js
@@ -1,101 +1,14 @@
 var app = new Vue({
     el: "#app",
     data: {
-        googleSearch: "",
+        googleSearch: '',
         cities: window.cities,
-        isActive: 0,
-        control: 0,
 
-        filteredCities:"",
-        update_filteredCities:true,
-        focused: false,
-        change: false,
-        inFocus: -1,
-        searchedInput:''
+        isInResults : false
     },
-    updated() {
-        this.$nextTick(() => {
-            if (this.googleSearch.length > 0) {        
-                this.$refs.second.focus();
-            } else {
-                this.$refs.first.focus();
-            }
-        });
-    },
-    watch:{
-        inFocus: function() {
-            this.update_filteredCities = false;
-            this.googleSearch=this.filteredCities[this.inFocus].name;
-        },
-        googleSearch: function() {
-            this.createFilteredCities(this.update_filteredCities);
-            this.update_filteredCities=true;
-            console.log(this.filteredCities);
-
-            if(this.inFocus == -1) {
-                this.searchedInput=this.googleSearch;
-            }
-        }
-    },
-    methods:{
-        zmiana: function(a){
-            if(this.isActive == 0)
-            {
-                this.isActive = 1;
-                this.googleSearch = a;
-                el2 = document.getElementById("autocom");
-                el2.blur();
-                this.control = 0;
-            }
-        },
-        pogrubienie: function(a)
-        {
-            wyszukaj = this.googleSearch;
-            var pom = a.split(wyszukaj);
-            for(i = 0; i < pom.length; i++)
-            {
-                a = a.replace(pom[i], pom[i].bold());
-            }
-            return a;
-        },
-        ustaw: function()
-        {
-            this.control = 1;
-        },
-        enter: function(){
-            this.update_filteredCities = true;
-            this.change = true;
-            this.inFocus = -1;
-            this.focused = false;
-        },
-        down: function(){
-            if(this.inFocus < this.filteredCities.length - 1) {
-                this.inFocus++; 
-            } else if(this.inFocus == this.filteredCities.length-1)  {
-                this.inFocus = 0; 
-            }
-        },
-        up: function(){
-            if(this.inFocus > 0) {
-                this.inFocus--; 
-            } else if(this.inFocus == 0) {
-                this.inFocus = this.filteredCities.length-1;
-            }
-        },
-        createFilteredCities: function(yes){
-            if(yes)
-            {
-                let result = this.cities.filter(city => city.name.includes(this.googleSearch));
-                if(result.length > 10)
-                {
-                    this.filteredCities = result.slice(1, 11);
-                }
-                else
-                {
-                    this.filteredCities = result;
-                }
-                this.inFocus = -1;
-            }   
+    methods : {
+        autocompleterSubmit(value) {
+            console.log('submit!', value);
         }
     }
 });
